4072a12c8634977da3787b6e72a452c5bf23f447
richard martins <richardx4000@hotmail.com>
2018-11-02 19:55:50 -0300

    02/11/2018 exercicios

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 "# uri-exercises-java"
 Exercicios em java do URI online judge (www.urionlinejudge.com.br)
+
+
+#### Observações
+*Se for subimeter no URI, alterar nome da classe para 'MAIN' e retirar caminho do package no topo do arquivo*
+*Os projetos foram feitos em java 8 embora a maioria rode em java 7*
--- uriexercicios/Uri1581_ConversaInternacional.java
+++ uriexercicios/Uri1581_ConversaInternacional.java
@@ -1,29 +1,32 @@
 package br.com.richardmartins.uriexercicios;
 
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Uri1581_ConversaInternacional {
-	public static void main(String[] args) {
-		Scanner ler = new Scanner(System.in);
-		int n = ler.nextInt();
-		for(int i = 0 ;i < n ; i ++){
-			int j = ler.nextInt();
-			String asdf = "";
-			for(int k = 0 ; k < j ; k++){
-				if(k == 0){
-					asdf = ler.next();
-				} else {
-					String sddf = ler.next();
-					if(sddf.equals(asdf)){
-						if(k == j-1){
-							System.out.println(asdf);
-						}
-					} else {
-						System.out.println("ingles");
-						break;
-					}
-				}
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        Scanner ler = new Scanner(System.in);
+
+        int n = ler.nextInt();
+        for (int i = 0; i < n; i++) {
+
+            int l = ler.nextInt();
+            List<String> lista = new ArrayList<>();
+            for (int j = 0; j < l; j++) {
+
+                String asdf = ler.next();
+
+                if (!lista.contains(asdf)) {
+                    lista.add(asdf);
+                }
+            }
+            if (lista.size() == 1) {
+                System.out.println(lista.get(0));
+            } else {
+                System.out.println("ingles");
+            }
+        }
+    }
 }
