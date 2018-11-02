07827e0a3b39f5ceb762d4994957d6dcaf3c9ca4
richard martins <richardx4000@hotmail.com>
2018-11-02 19:06:53 -0300

    02/11/2018 exercicios

---
+++ uriexercicios/Uri1070_SeisNumerosImpares.java
@@ -0,0 +1,21 @@
+package br.com.richardmartins.uriexercicios;
+
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Uri1070_SeisNumerosImpares {
+
+    public static void main(String[] args) throws IOException {
+        Scanner ler = new Scanner(System.in);
+        int i = 0;
+        int n = ler.nextInt();
+
+        while (i < 6) {
+            if (n % 2 != 0) {
+                i++;
+                System.out.println(n);
+            }
+            n++;
+        }
+    }
+}
